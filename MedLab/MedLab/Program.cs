8346aaed1e48984398d715aaa70bf97e776a86e8
NareshLokhande49@gmail.com <nareshlokhande49@gmail.com>
2024-08-13 10:18:48 +0530

    "Updated Backend WIth JWT and Identity, Updated README"

--- MedLab/MedLab/Program.cs
+++ MedLab/MedLab/Program.cs
@@ -1,10 +1,11 @@
+
+using MedLab.Constants;
 using MedLab.Data;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Abstractions;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.Resource;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace MedLab
 {
@@ -15,18 +16,65 @@ namespace MedLab
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
-            builder.Services.AddDbContext<MajorMedLabContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("MajorMedLabContext")));
+            
+
+            builder.Services.AddDbContext<MedLabDatabaseContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("MedLabDBContext")));
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+           
+            builder.Services.AddAuthorization();
+            builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+                .AddEntityFrameworkStores<MedLabDatabaseContext>();
+
+            /*           builder.Services
+                             .AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                             .AddEntityFrameworkStores<MedLabDatabaseContext>()
+                             .AddDefaultTokenProviders();
+
+                        builder.Services.AddAuthentication(options =>
+                        {
+                            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                        })
+                            .AddJwtBearer(options =>
+                            {
+                                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                                {
+                                    ValidateIssuer = true,
+                                    ValidateAudience = false,
+                                    ValidateLifetime = true,
+                                    ValidateIssuerSigningKey = true,
+                                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key!"]))
+                                };
+                            });
+
+                        builder.Services.AddAuthorization(options =>
+                        {
+                            options.AddPolicy("AdminPolicy", policy =>
+                                policy.RequireAssertion(context =>
+                                    context.User.HasClaim(c => c.Type == "Role" && c.Value == Role.ADMIN.ToString())));
+
+                            options.AddPolicy("LabAssistantPolicy", policy =>
+                                policy.RequireAssertion(context =>
+                                    context.User.HasClaim(c => c.Type == "Role" && c.Value == Role.LABASSISTANT.ToString())));
+
+                            options.AddPolicy("PatientPolicy", policy =>
+                                policy.RequireAssertion(context =>
+                                    context.User.HasClaim(c => c.Type == "Role" && c.Value == Role.PATIENT.ToString())));
+                        });*/
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            app.MapIdentityApi<IdentityUser>();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -36,6 +84,8 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("MajorMedLabConte
             }
 
             app.UseHttpsRedirection();
+            /*app.UseAuthentication();*/
+
 
             app.UseAuthorization();
 
